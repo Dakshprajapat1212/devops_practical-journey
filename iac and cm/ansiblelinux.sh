5e342c2220cac7e9ac16268a879fafa86b019de8
Dakshprajapat1212 <2023csdaksh13685@poornima.edu.in>
2025-08-18 13:17:53 +0530

    Create ansiblelinux.sh

---
+++ iac and cm/ansiblelinux.sh
@@ -0,0 +1,22 @@
+Generating public/private ed25519 key pair.
+Enter file in which to save the key (/home/ubuntu/.ssh/id_ed25519): y
+Enter passphrase (empty for no passphrase):
+Enter same passphrase again:
+Your identification has been saved in y
+Your public key has been saved in y.pub
+The key fingerprint is:
+SHA256:ZUYgMzJCU2Guv70NN4CPoZ6BXtDEPNqrf40FfAVBCZU ubuntu@ip-172-31-38-189
+The key's randomart image is:
++--[ED25519 256]--+
+| .+.*oB=*..      |
+|  o= o E o       |
+|   =o   . +      |
+|  =..+ . +       |
+| o.oo + S        |
+| ..o.+ o         |
+|. ooo * o        |
+|..oo = = .       |
+| o+.o o..        |
++----[SHA256]-----+
+ubuntu@ip-172-31-38-189:~$ ls /home/ubuntu/.ssh/
+authorized_keys  id_ed25519  id_ed25519.pub  known_hosts
